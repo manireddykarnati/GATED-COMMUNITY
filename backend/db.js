2295622bcb37e217d56b62c355d66c5ec6bbdcca
Mani Gnanendra Reddy <manignanendrareddy@Manis-MacBook-Air.local>
2025-05-20 11:36:29 +0530

    updated css

--- backend/db.js
+++ backend/db.js
@@ -2,18 +2,18 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'gcms',
-  password: 'chandan@2710',
-  port: 5432,
+    user: 'postgres',
+    host: 'localhost',
+    database: 'org',
+    password: 'mani123',
+    port: 5433,
 });
 
 pool.connect()
-  .then(() => console.log("✅ Connected to PostgreSQL DB"))
-  .catch((err) => {
-    console.error("❌ DB connection error:", err);
-    process.exit(1);
-  });
+    .then(() => console.log("✅ Connected to PostgreSQL DB"))
+    .catch((err) => {
+        console.error("❌ DB connection error:", err);
+        process.exit(1);
+    });
 
 module.exports = pool;
